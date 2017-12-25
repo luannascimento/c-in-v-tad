c18d5a407fbcfb9b98a61be2ce7cfa538625a8cf
Luan Nascimento <rafaelluan100@gmail.com>
2017-12-25 16:57:36 -0200

    add função match, valida uma expressão regular

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # c-in-v-tad
 Leitura e validação de dados em c.
+
+<h2>Valid</h2>
+Compilação e validação de expressão regular
+protótipo
+<i>static int match( char* str, char* regex );</i>
--- valid.c
+++ valid.c
@@ -1,1 +1,20 @@
 #include "libs/headers.h"
+
+//função responsavel por fazer a validação da explessão regular
+static int match( char* str, char* regex )
+{
+    regex_t reg;
+	
+    if( regcomp(&reg, regex, REG_EXTENDED|REG_NOSUB ) != 0 )
+    {
+	fprintf(stdout,"Erro expressão regular invalida\n");
+	exit(1);
+    }
+				
+    if( (regexec( &reg, str, 0, (regmatch_t*)NULL, 0)) != 0 )
+    {
+	return 0;
+    }
+						    
+    return 1;
+}
